dab41f361e244bc7399384001bad0f5d4370f6f0
Joe <josephchang@outlook.com>
2017-08-23 16:03:26 +0100

    Tile notification for Windows 10 using React Native

---
+++ ReactNativeNotificationPackage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace ReactNativeNotification
+{
+    public class ReactNativeNotificationPackage : IReactPackage
+    {
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig ()
+        {
+            return new List<Type>(0);
+        }
+
+        public IReadOnlyList<INativeModule> CreateNativeModules (ReactContext reactContext)
+        {
+            return new List<INativeModule> {
+                new TileNotificationModule(reactContext)
+            };
+        }
+
+        public IReadOnlyList<IViewManager> CreateViewManagers (ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
---
+++ TileNotificationModule.cs
@@ -0,0 +1,40 @@
+﻿using ReactNative.Bridge;
+using System.Text;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace ReactNativeNotification
+{
+    public class TileNotificationModule : ReactContextNativeModuleBase
+    {
+        public TileNotificationModule (ReactContext reactContext)
+            : base(reactContext) 
+        {
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "TileNotification";
+            }
+        }
+
+        [ReactMethod]
+        public void showTileNotification(string notificationXml)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.LoadXml(notificationXml);
+
+            TileNotification notification = new TileNotification(xdoc);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(notification);
+        }
+
+        [ReactMethod]
+        public void clearTileNotification()
+        {
+            TileUpdateManager.CreateTileUpdaterForApplication().Clear();
+        }
+    }
+}
